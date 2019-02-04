c5c28d72bd7ae0da00ff2d09337315f4dbc6c7e2
Raymond Huynh <raymond.h@myplanet.com>
2019-02-03 19:51:40 -0500

    adds binary search

--- algorithms/search.js
+++ algorithms/search.js
@@ -7,3 +7,21 @@ exports.linearSearch = function linearSearch(arr, value) {
   } 
   return -1;
 }
+
+function binarySearch(arr, value) {
+  let start = 0;
+  let end = arr.length - 1;
+  while (start <= end) {
+    const middle = Math.floor((start + end) / 2);
+    const currentValue = arr[middle];
+    if (currentValue === value) {
+      return middle;
+    } else if (currentValue > value) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+  }
+  return -1;
+}
+
